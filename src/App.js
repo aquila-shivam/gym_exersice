314e0b40d9c3695d843fd6ea0c88ab6d5e4f6990
aquila-shivam <shivam.52.nishad@gmail.com>
2022-06-17 10:11:38 +0000

    files

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import {Box} from '@mui/material';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import ExerciseDetails from './pages/ExerciseDetails';
+import Footer from './components/Footer';
 
 const App=() =>{
 return(
   
-  <Box>
+  <Box width="400px" sx={{width:{xl:'1488px'}}} m="auto">
     <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/element/:id" element={<ExerciseDetails/>}/>
     </Routes>
+    <Footer/>
   </Box>
 )
 }
