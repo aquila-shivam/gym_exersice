a16987b81b1fbd95953a8ee349386f5f8e2dc418
aquila-shivam <shivam.52.nishad@gmail.com>
2022-06-17 08:40:23 +0000

    file system

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@
 import './App.css';
 import {Routes,Route} from 'react-router-dom';
 import {Box} from '@mui/material';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ExerciseDetails from './pages/ExerciseDetails';
 
 const App=() =>{
 return(
+  
   <Box>
+    <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/element/:id" element={<ExerciseDetails/>}/>
